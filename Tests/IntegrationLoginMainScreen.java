e70050855b71cde6d25881632620e7443eeda502
danieel47896321 <74861262+danieel47896321@users.noreply.github.com>
2021-05-09 13:35:33 +0300

    Update IntegrationLoginMainScreen.java

--- Tests/IntegrationLoginMainScreen.java
+++ Tests/IntegrationLoginMainScreen.java
@@ -1,6 +1,12 @@
 package com.example.socialprojectsce;
 
+import androidx.annotation.NonNull;
+
 import com.example.socialprojectsce.Classes.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -12,23 +18,42 @@ import org.junit.Test;
 public class IntegrationLoginMainScreen {
     private FirebaseDatabase database = FirebaseDatabase.getInstance();
     private DatabaseReference reference = database.getReference().child("Users");
+    private FirebaseAuth myAuth = FirebaseAuth.getInstance();
     @Test
-    public boolean validateLoginUserInformation(String email){
+    public boolean validateLoginUserInformation(String email,String Password){
         //from user
         User user1 = new User();
         user1.setEmail(email);
         //from db
         User user2 = new User();
-        reference.orderByChild("email").equalTo(email).addListenerForSingleValueEvent(new ValueEventListener() {
+        myAuth.signInWithEmailAndPassword(email,Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                for(DataSnapshot datas: dataSnapshot.getChildren()){
-                    user2.setEmail(datas.child("email").getValue().toString());
-                }
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    reference.orderByChild("email").equalTo(email).addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            for(DataSnapshot datas: dataSnapshot.getChildren()){
+                                user2.setAge(datas.child("age").getValue().toString());
+                                user2.setCity(datas.child("city").getValue().toString());
+                                user2.setDepart(datas.child("depart").getValue().toString());
+                                user2.setEmail(datas.child("email").getValue().toString());
+                                user2.setFirstname(datas.child("firstname").getValue().toString());
+                                user2.setFlag(datas.child("flag").getValue().toString());
+                                user2.setLastname(datas.child("lastname").getValue().toString());
+                                user2.setPhone(datas.child("phone").getValue().toString());
+                                user2.setSex(datas.child("sex").getValue().toString());
+                                user2.setType(datas.child("type").getValue().toString());
+                            }
+                        }
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) { }
+                    });
+                } else{  }
             }
-            @Override
-            public void onCancelled(DatabaseError databaseError) { }
         });
         return user1 == user2;
     }
 }
+
+
