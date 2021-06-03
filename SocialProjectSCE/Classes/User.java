fcf36539472504de991d760ec79bc609b368587f
YaromCh <58593095+YaromCh@users.noreply.github.com>
2021-06-03 17:12:43 +0300

    finish
    finish

--- SocialProjectSCE/Classes/User.java
+++ SocialProjectSCE/Classes/User.java
@@ -1,8 +1,33 @@
 package com.example.socialprojectsce.Classes;
 
+import com.example.socialprojectsce.R;
+
 import java.io.Serializable;
+import java.util.Objects;
 
 public class User implements Serializable {
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "Photo=" + Photo +
+                ", Email='" + Email + '\'' +
+                ", Phone='" + Phone + '\'' +
+                ", Type='" + Type + '\'' +
+                ", Age='" + Age + '\'' +
+                ", City='" + City + '\'' +
+                ", Sex='" + Sex + '\'' +
+                ", Depart='" + Depart + '\'' +
+                ", Firstname='" + Firstname + '\'' +
+                ", Lastname='" + Lastname + '\'' +
+                ", Flag='" + Flag + '\'' +
+                ", Study='" + Study + '\'' +
+                ", Year='" + Year + '\'' +
+                ", Semester='" + Semester + '\'' +
+                '}';
+    }
+
+    private int Photo = R.drawable.ic_student;;
     private String Email;
     private String Phone;
     private String Type; //0-student 1-teacher 2-admin
@@ -13,6 +38,10 @@ public class User implements Serializable {
     private String Firstname;
     private String Lastname;
     private String Flag;
+    private String Study=null;
+    private String Year=null;
+    private String Semester=null;
+    private String background="background";
     public User() {}
 
         //ctor
@@ -28,7 +57,6 @@ public class User implements Serializable {
         Lastname = lastname;
         Flag = flag;
     }
-    //getters
     public String getEmail() { return Email; }
     public String getType() { return Type; }
     public String getAge() { return Age; }
@@ -39,8 +67,16 @@ public class User implements Serializable {
     public String getLastname() { return Lastname; }
     public String getFlag() { return Flag; }
     public String getPhone() { return Phone; }
-
-    //setters
+    public String getStudy() { return Study; }
+    public String getYear() { return Year; }
+    public String getSemester() { return Semester; }
+    public String getBackground() { return background; }
+    public void setBackground(String background) { this.background = background; }
+    public int getPhoto() { return Photo; }
+    public void setPhoto(int photo) { Photo = photo; }
+    public void setStudy(String study) { Study = study; }
+    public void setYear(String year) { Year = year; }
+    public void setSemester(String semester) { Semester = semester; }
     public void setPhone(String phone) { Phone = phone; }
     public void setEmail(String email) { Email = email; }
     public void setType(String type) { Type = type; }
@@ -51,4 +87,21 @@ public class User implements Serializable {
     public void setFirstname(String firstname) { Firstname = firstname; }
     public void setLastname(String lastname) { Lastname = lastname; }
     public void setFlag(String flag) { Flag = flag; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return o.equals(Email) &&
+                o.equals(Phone) &&
+                o.equals(Type) &&
+                o.equals(Age) &&
+                o.equals(City) &&
+                o.equals(Sex) &&
+                o.equals(Depart) &&
+                o.equals(Firstname) &&
+                o.equals(Lastname) &&
+                o.equals(Flag);
+    }
 }
