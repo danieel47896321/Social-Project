e4e134c38bbadfd7397e748ba520b36857c061e1
YaromCh <58593095+YaromCh@users.noreply.github.com>
2021-06-03 17:07:04 +0300

    finish
    finish

--- SocialProjectSCE/TagsFuncs/Studies.java
+++ SocialProjectSCE/TagsFuncs/Studies.java
@@ -1,6 +1,7 @@
 package com.example.socialprojectsce.TagsFuncs;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -15,6 +16,11 @@ import com.example.socialprojectsce.Classes.Tags;
 import com.example.socialprojectsce.Classes.User;
 import com.example.socialprojectsce.R;
 import com.example.socialprojectsce.Classes.TagsView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.sql.ResultSet;
 import java.util.ArrayList;
@@ -27,46 +33,54 @@ public class Studies extends AppCompatActivity {
     private RecyclerView viewList;
     private Intent intent;
     private List<Tags> tags;
-    private ResultSet resultSet;
-    /*
-    private static String TAG = null;
-    private static String Engn = null;
-    private static String Year = null;
-    private static String Semester = null;
-    private String Eng[] = {"הנדסת תוכנה","הנדסת חשמל","הנדסת תעשייה וניהול","הנדסת בנין","הנדסת מכונות","הנדסת כימיה","הנדסת תקשורת"};
-    private String Years[] = {"מכינה","שנה א","שנה ב","שנה ג","שנה ד"};
-    private String Sem[] = {"סמסטר קיץ","סמסטר א","סמסטר ב"};*/
+    private View screenView;
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference reference = database.getReference().child("Studies");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_studies);
         init();
     }
     public void init(){
-        setContentView(R.layout.activity_studies);
         tags = new ArrayList<>();
+        screenView = findViewById(R.id.rView);
+
         btnBack = findViewById(R.id.Back);
         viewList = findViewById(R.id.StudiesRV);
         TagText = findViewById(R.id.TagText1);
         intent = getIntent();
         user = (User)intent.getSerializableExtra("user");
+        if(user.getBackground().equals("background"))
+            screenView.setBackground(ContextCompat.getDrawable(getApplicationContext(),R.drawable.background));
+        else if(user.getBackground().equals("background1"))
+            screenView.setBackground(ContextCompat.getDrawable(getApplicationContext(),R.drawable.background1));
+        else if(user.getBackground().equals("background2"))
+            screenView.setBackground(ContextCompat.getDrawable(getApplicationContext(),R.drawable.background2));
+        else if(user.getBackground().equals("background3"))
+            screenView.setBackground(ContextCompat.getDrawable(getApplicationContext(),R.drawable.background3));
         SetTags();
         Back();
     }
     public void SetTags(){
-        try {
-           // resultSet = db.getCategory("לימודים");
-            if (resultSet.next())
-                do {
-                    String tagname = resultSet.getString("Name");
-                    String category = resultSet.getString("Category");
-                    int photo = resultSet.getInt("Photo");
+        reference.orderByChild("category").equalTo("Studies").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot datas: dataSnapshot.getChildren()){
+                    String tagname = datas.child("text").getValue().toString();
+                    String category = datas.child("category").getValue().toString();
+                    int photo = Integer.valueOf(datas.child("photo").getValue().toString());
                     tags.add(new Tags(tagname, category, photo));
-                } while (resultSet.next());
-        }
-        catch (Exception e){
-            Toast.makeText(Studies.this, "Failed", Toast.LENGTH_LONG).show();
-        }
+                }
+                ShowTags(tags);
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) { }
+        });
+    }
+    public void ShowTags(List<Tags> tagList){
         TagsView myTags = new TagsView(this,tags);
+        myTags.setUser(user);
         viewList.setLayoutManager(new GridLayoutManager(this,3));
         viewList.setAdapter(myTags);
     }
